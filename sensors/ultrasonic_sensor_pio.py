cded97c697d7410303675c1f6c7061a8a276024c
Robin Heitz <robin.heitz@rwth-aachen.de>
2023-09-28 20:09:11 +0200

    Working distance measurement with PIO!

--- sensors/ultrasonic_sensor_pio.py
+++ sensors/ultrasonic_sensor_pio.py
@@ -18,6 +18,7 @@ ECHO        -> Input pin
 TRIGGER_GPIO = 28
 ECHO_GPIO = 22
 SOUND_VELOCITY_M_S = 343
+FREQUENCY = 10_000_000
 
 micropython.alloc_emergency_exception_buf(100)
 
@@ -26,7 +27,10 @@ micropython.alloc_emergency_exception_buf(100)
 def measure_distance():
     wrap_target()
 
-    set(pins, 1) [19]     # lasts 10 us
+    set(pins, 1) [29]     # lasts 10 us
+    nop() [29]
+    nop() [29]
+    nop() [9]
     set(pins, 0)
     mov(y, 31)           # y's value is: 4_294_967_295
     wait(1, pin, 0)        # wait until input pin at location 0 is HIGH
@@ -38,18 +42,27 @@ def measure_distance():
     push()
     wrap()
 
+
+def calc_dist_m(time_sec_both_ways):
+    return SOUND_VELOCITY_M_S * time_sec_both_ways / 2
+
+
 def main(sm:rp2.StateMachine):
     """Read the output FIFO."""
 
     while True:
         
-        diff = 2**32 - 1 - sm.get()
+        diff_step = 2**32 - 1 - sm.get() # 2^32 -1 is the start value of the state machine's register
+        # each cycle lasts 1/freq = 0.5 us
+        # We have 2 cycles per diff_step
+        echo_time = diff_step * 2 * (1 / FREQUENCY)  # diff_steps * num_cycles_per_step * cycle_time 
+        dist_m = calc_dist_m(echo_time)
 
-        print(diff)
+        print(dist_m)
         time.sleep(0.1)
 
 if __name__ == "__main__":
     rp2.PIO(0).remove_program()
-    sm = rp2.StateMachine(0, measure_distance, freq=2_000_000, set_base=Pin(TRIGGER_GPIO, Pin.OUT), in_base=Pin(ECHO_GPIO, Pin.IN), jmp_pin=Pin(ECHO_GPIO))
+    sm = rp2.StateMachine(0, measure_distance, freq=FREQUENCY, set_base=Pin(TRIGGER_GPIO, Pin.OUT), in_base=Pin(ECHO_GPIO, Pin.IN), jmp_pin=Pin(ECHO_GPIO))
     sm.active(1)
     main(sm)
